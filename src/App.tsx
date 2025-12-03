4144f360e794dbeefe3b898d436ebf6e66f5e3d4
Robert Livingston <rll@bu.edu>
2025-12-03 15:13:27 -0600

    Update email

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ const App: React.FC = () => {
             <strong>Contact Us:</strong>
             <br />
             <a
-              href="mailto:mysticmanamarket@gmail.com"
+              href="mailto:MysticManaMart@gmail.com"
               className="contact-email"
             >
               mysticmanamarket@gmail.com
